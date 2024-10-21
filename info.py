457c579f1c8567617775423f3432b7e1073ac764
Ellisamovies <121487082+Ellisamovies@users.noreply.github.com>
2024-10-21 16:02:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -53,7 +53,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "vkbotsthalpathy")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-VERIFY = bool(environ.get('VERIFY', False))
+VERIFY = bool(environ.get('VERIFY',False))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'kingurl.in')
 SHORTLINK_API = environ.get('SHORTLINK_API', 'b6ab0e7adb6457e594eeb55a07276703cfda7bb8')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
